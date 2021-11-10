b97f86cc4f7402e41642dcab4e1194c71aa370c2
Bekham <anton-bekker@yandex.ru>
2021-11-10 16:48:19 +0600

    1

--- telegram_bot.py
+++ telegram_bot.py
@@ -42,3 +42,4 @@ if __name__ == '__main__':
   loop = asyncio.get_event_loop()
   loop.create_task(scheduled(PARSE_TIME_HOURS*60*60))
   executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
