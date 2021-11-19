1e0847d4e2e8683f5a10d5a65682fd12a4615b56
Bekham <anton-bekker@yandex.ru>
2021-11-19 13:43:43 +0600

    12_11

--- create_bot.py
+++ create_bot.py
@@ -3,6 +3,10 @@ from aiogram.dispatcher import Dispatcher
 import json
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import logging
+import os
+
+from google.cloud import dialogflow
+
 logging.basicConfig(level=logging.INFO)
 
 with open('login.json', 'r') as f:
@@ -10,7 +14,13 @@ with open('login.json', 'r') as f:
 TOKEN = login_data['TOKEN']
 
 storage = MemoryStorage
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="service-account-file.json"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+session_client = dialogflow.SessionsClient() #Сессия клиента
+project_id = 'small-talk-lmsc' #Айди проекта берём с json файла
+session_id = 'sessions' #Указываем любое значение, в моём случае "sessions"
+language_code = 'ru' #Язык русский
+session = session_client.session_path(project_id, session_id) #Объявляем сессию по айди проекта и айди сессии
--- handlers/other.py
+++ handlers/other.py
@@ -1,16 +1,38 @@
 from aiogram import types, Dispatcher
+import json
 
+from google.cloud import dialogflow
+
+from create_bot import language_code, session_client, session, bot
 from data_base import sqlite_db
+# from dialogflow import types
+# from google.cloud import dialogflow
+import os
+# import google.cloud.dialogflow
+from google.api_core.exceptions import InvalidArgument
 
 
-# async def menu_command(message: types.Message):
-#     await sqlite_db.sql_read(message)
 
 async def echo_send(message: types.Message):
-    if message.text == 'Привет' or message.text == 'привет':
-        await message.answer('Игорь пидр!')
-    else:
-        await message.answer(message.text)
+    text_input = dialogflow.TextInput(  # Текст запроса
+        text=message.text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)  # Ввод запроса
+    response = session_client.detect_intent(  # Ответ бота
+        session=session, query_input=query_input)
+    if response.query_result.fulfillment_text:  # Если ответ имеется
+        await bot.send_message(message.from_user.id,
+                               response.query_result.fulfillment_text)  # Отправляем его пользователю
+    else:  # В обратном случае
+        await bot.send_message(message.from_user.id, "Я тебя не понимаю")  # Я тебя не понимаю
+    # if response:
+    #     await message.answer(text=response)
+    # else:
+    #     await message.answer('Я Вас не совсем понял!')
+
+    # if message.text == 'Привет' or message.text == 'привет':
+    #     await message.answer('Игорь пидр!')
+    # else:
+    #     await message.answer(message.text)
     # await message.reply(message.text)
     #     await bot.send_message(message.from_user.id, message.text)
 
